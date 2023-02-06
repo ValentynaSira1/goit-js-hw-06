1881a77a7fcec66910363f618cc1500c7dbcbc92
Валентина Сіра <113180300+ValentynaSira1@users.noreply.github.com>
2023-02-06 22:42:17 +0200

    внесення змін у 8 завданні

--- js/task-08.js
+++ js/task-08.js
@@ -10,6 +10,6 @@ function handleSubmit(event) {
   if (email.value === "" || password.value === "") { 
     return alert("Please fill in all the fields!"); 
   } 
-      console.log(`Login: ${email.value}, Password: ${password.value}`); 
+  console.log({login: email.value, password: password.value}); 
   event.currentTarget.reset(); 
 }
